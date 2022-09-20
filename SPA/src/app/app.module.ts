ecb50fa4991f0ddf7026bf5130a12a7f4f440998
khanhtung97 <nguyenkhanhtung580@gmail.com>
2022-09-20 09:06:10 +0700

    add ngx-bootrap

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { FormsModule } from '@angular/forms';
 import { AuthService } from './_core/_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 @NgModule({
   declarations: [				
     AppComponent,
@@ -18,7 +20,9 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [
     AuthService,
